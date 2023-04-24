1851c918c52c2b53dae6a19e4be9cb82baae8966
GabiMaj <gabriel.majluf@ices.edu.ar>
2023-04-24 19:50:09 -0300

    c1

---
+++ EJERCICIOS CLASE/EjerciciosClase/EjerciciosRepaso1/Repaso2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosRepaso1
+{
+    internal class Repaso2
+    {
+    }
+}
